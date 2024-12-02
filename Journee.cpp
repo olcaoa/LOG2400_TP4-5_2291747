94db5547907ad599bc19f77a4c84694a336fe2d0
olcaoa <144279778+olcaoa@users.noreply.github.com>
2024-12-01 23:41:45 -0500

    Journee h et cpp

--- Journee.cpp
+++ Journee.cpp
@@ -1,6 +1,33 @@
 #include "Journee.h"
+#include <sstream>
 
-std::string Journee::imprimer()
-{
-    return "reservation: \n" + imprimerEnfants();
+// Constructeur
+Journee::Journee(int mois, int jour, int annee) : _mois(mois), _jour(jour), _annee(annee) {
+    _nom = "Journee " + getDate();
+    _description = "Réservations pour la journée " + getDate();
+}
+
+// Accesseurs pour la date
+int Journee::getMois() const {
+    return _mois;
+}
+
+int Journee::getJour() const {
+    return _jour;
+}
+
+int Journee::getAnnee() const {
+    return _annee;
+}
+
+std::string Journee::getDate() const {
+    std::ostringstream oss;
+    oss << _mois << "/"
+        <<  _jour << "/"
+        << _annee;
+    return oss.str();
+}
+
+std::string Journee::imprimer() {
+    return "Date: " + getDate() + "\nRéservations :\n" + imprimerEnfants();
 }
--- Journee.h
+++ Journee.h
@@ -1,11 +1,23 @@
 #pragma once
 #include "AbstractReservationComposite.h"
 #include "Reservation.h"
+#include <vector>
+#include <string>
 
 class Journee : public ReservationComposite
 {
 private:
-	std::vector<Reservation*> _reservations;
-protected:
-	std::string imprimer() override;
+    int _mois;
+    int _jour;
+    int _annee;
+
+public:
+    Journee(int mois, int jour, int annee);
+
+    int getMois() const;
+    int getJour() const;
+    int getAnnee() const;
+    std::string getDate() const;
+
+    std::string imprimer() override;
 };
